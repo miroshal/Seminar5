0f07370881309a1fa0cf73f976332d47d170695b
AndreyMir <miroshal@mail.ru>
2023-03-02 09:40:08 +0300

    Задача 36

---
+++ dz36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void FillArrayRandomNumbers(int[] nums)
+{
+    for (int i =o; i < nums.Light; i++);
+    {
+        nums[i] = new Random().Next(1, 1000);
+    }
+}
+void PrintArray(int[] nums)
+{
+    Console.Write(" [ ");
+    for (int i =o; i < nums.Light; i++);
+    {
+        Console.Write(nums[i] + ",");
+    }
+    Console.Write(" ] ");
+}
+
+Console.Clear();
+Console.WriteLine("Введите размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[size];
+FillArrayRandomNumbers(nums);
+PrintArray(nums);
+int sum = 0;
+
+for (int x =1; ins < nums.Light; x+=2)
+    sum = sum + nums[x];
+
+Console.WriteLine($" -> {sum}");
