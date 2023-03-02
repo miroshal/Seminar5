77dd99b4ff7fe03055c3ec2f4179e25d0bec4bbb
AndreyMir <miroshal@mail.ru>
2023-03-02 09:56:34 +0300

    Задача 38

---
+++ dz38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+Console.WriteLine("Введите размер массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] nums = new double[size];
+FillArrayRandomNumbers(nums);
+PrintArray(nums);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x =0; x < nums.Light; x++)
+{
+    if (nums[X] > max) max = nums[x];
+    if (nums[x] < min) min = nums[x];
+}
