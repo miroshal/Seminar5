a3fea1fe5e58d577cbdf78a77863f64cc17a515c
AndreyMir <miroshal@mail.ru>
2023-03-02 09:06:08 +0300

    Задача 34

--- dz34/Program.cs
+++ dz34/Program.cs
@@ -6,4 +6,10 @@ for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(99, 1000);
     Console.Write($"{array[i]}");
-}    
+}   
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0 ) count++;
+}
+Console.WriteLine($"-> {count}");
